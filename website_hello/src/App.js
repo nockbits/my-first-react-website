d8ec6a3a438fd9ee2b7a789a8c4183b31f15cbc6
Austin Noronha <austin_noronha@hotmail.com>
2020-10-24 18:02:58 +0530

    Updated the sample website

--- website_hello/src/App.js
+++ website_hello/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2>
           Hello world!
-        </p>
-        <legend>Welcome to your first React JS Website</legend>
+        </h2>
+        <p>Welcome to your first React JS Website</p>
         
         <ol>
           <li>
